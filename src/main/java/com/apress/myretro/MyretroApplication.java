38d183807fa315db8fd432b63cd46d38f160c202
Proxy539 <roman.filiyutich@gmail.com>
2026-09-01 14:05:14 +0300

    Complete chapter 2

---
+++ src/main/java/com/apress/myretro/MyretroApplication.java
@@ -0,0 +1,17 @@
+package com.apress.myretro;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyretroApplication {
+
+    static Logger log = LoggerFactory.getLogger(MyretroApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyretroApplication.class, args);
+
+    }
+}
